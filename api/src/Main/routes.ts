688b8989cb7e431224ea23c2de939fcccbc99041
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2024-03-14 14:37:56 -0700

    fix: database setup, migration and seed

--- api/src/Main/routes.ts
+++ api/src/Main/routes.ts
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Router } from 'express';
 
-import { STRApplicationRoutes } from '../Modules/STR-applications/Main/routes';
-
 export const routes = Router();
 
 routes.get('/healthcheck', (_, res) => res.json({ status: 'ok' }));
-
-routes.use('/v1/str-applications', STRApplicationRoutes);
