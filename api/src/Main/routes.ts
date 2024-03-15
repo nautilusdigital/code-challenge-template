722a6e9c5191bea450e0617ccbf13c8fc851c941
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2024-03-14 20:47:49 -0700

    feat: added user routes

--- api/src/Main/routes.ts
+++ api/src/Main/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { contactRoutes } from '../Modules/Contacts/Main/routes';
 import { ReportRoutes } from '../Modules/Reports/Main/routes';
 import { caseRoutes } from '../Modules/Cases/Main/routes';
+import { usersRoutes } from '../Modules/Users/Main/routes';
 
 export const routes = Router();
 
@@ -11,3 +12,4 @@ routes.use('/v1/contact', contactRoutes);
 
 routes.use('/v1/reports', ReportRoutes);
 routes.use('/v1/case', caseRoutes);
+routes.use('/v1/user', usersRoutes);
